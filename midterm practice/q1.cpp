84921200254c247b2c10cbe0460a730cbf4f9fcb
Gary <galvarado1501@gmail.com>
2025-03-22 16:03:18 -0700

    finished questions

--- midterm practice/q1.cpp
+++ midterm practice/q1.cpp
@@ -1,278 +1,229 @@
 /*
-
 Fill in the missing code for an OrderedLinkedList class.The values in an OrderedLinkedList are always in either ascending order or flat, starting with the smallest value in the head node.
-
 Copy paste your edited file, You don't need to include a sample run
-
 */
 
 #include <iostream>
-
 #include <stdexcept>
-
 using namespace std;
 
-
-
-
 template <class T>
-
 class Node {
-
 public:
-
     T value;
-
     Node* prev, * next;
-
     Node(const T& v, Node* p = nullptr, Node* n = nullptr) {
-
         prev = p; next = n; value = v;
-
     }
-
 };
 
 
-
-
-
-
-
-// A doubly linked list in which the values are arranged in ascending order.
-
 template <class T>
-
 class OrderedLinkedList {
-
     friend ostream& operator<<(ostream& out, const  OrderedLinkedList<T>& l) {
-
         out << "{ ";
-
-        // FILL IN MISSING CODE
-
-        cout << "}";
+        Node<T>* temp = l.headPtr;
+        while (temp != nullptr) {
+            if (temp->next != nullptr)
+                out << temp->value << ", ";
+            else out << temp->value;
+            temp = temp->next;
+        }
+        out << "}";
 
         return out;
-
     }
 
 private:
-
     Node<T>* headPtr, // a pointer to first node or null if list is empty.
-
         * tailPtr; // a pointer to last  node or null if list is empty.
-
     int length; // the list length
 
 
-
-
-    // Return a pointer to first node containing v
-
-    // or return nullptr if v is not found.
-
     Node<T>* findNodeWithValue(const T& v) const {
-
-        // FILL IN MISSING CODE
-
+        Node<T>* temp = headPtr;
+        while (temp != nullptr) {
+            if (temp->value == v) return temp;
+            temp = temp->next;
+        }
+        return nullptr;
     }
 
 public:
-
-
-
-
-    // Set headPtr and nullPtr to null and set length to zero.
-
     OrderedLinkedList() {
-
-        // FILL IN MISSING CODE
-
+        headPtr = nullptr;
+        tailPtr = nullptr;
+        length = 0;
     }
 
-
-
-
-    // Copy constructor.
 
     OrderedLinkedList(const OrderedLinkedList& other) {
-
-        // FILL IN MISSING CODE
-
-
-
+        if (other.headPtr == nullptr) {
+            headPtr = nullptr;
+            length = 0;
+            return;
+        }
+        headPtr = new Node<T>(other.headPtr);
+        Node<T>* temp = other.headPtr->next;
+        Node<T>* current = headPtr;
+        while (temp != nullptr) {
+            current->next = new Node<T>(temp);
+            current->prev->next = current;
+            current = current->next;
+            temp = temp->next;
+        }
+        tailPtr = current;
+        length = other.length;
     }
 
-
-
-
-    // Return value in node at given index
-
-    // or throw logic_error if index < 0 or > length-1
-
-    // Message should be "Index out of bounds"
 
     T& operator[](int index) {
-
         if (index < 0 || index > length - 1)
-
             throw logic_error("Index out of bounds");
 
-        // FILL IN MISSING CODE
-
+        else {
+            Node<T>* temp = headPtr;
+            int count = 0;
+            while (count < index && temp != nullptr) {
+                count++;
+                temp = temp->next;
+            }
+            if (temp != nullptr) return temp->value;
+        }
+        throw logic_error("Index out of bounds");
     }
 
-
-
-
-    // Destructor
 
     ~OrderedLinkedList() {
-
-        // FILL IN MISSING CODE
-
-
-
-
+        Node<T>* temp = headPtr;
+        while (temp != nullptr){
+            Node<T>* toDelete = temp;
+            temp = temp->next;
+            delete toDelete;
+        }
+        headPtr = nullptr;
+        tailPtr = nullptr;
+        length = 0;
     }
 
-
-
-
-    // Remove all values from the list.
 
     void clear() {
-
-        // FILL IN MISSING CODE
-
-
+        //~OrderedLinkedList();
+        Node<T>* temp = headPtr;
+        while (temp != nullptr) {
+            Node<T>* toDelete = temp;
+            temp = temp->next;
+            delete toDelete;
+        }
+        headPtr = nullptr;
+        tailPtr = nullptr;
+        length = 0;
     }
 
-
-
-
-    // Return the number of occurrences of v in the list
-
-    // or return 0 if v is not found.
-
-    // Your code should call findNodeWithValue.
 
     int count(const T& v) {
-
         Node<T>* p = findNodeWithValue(v);
-
-        // FILL IN MISSING CODE
-
+        if (p == nullptr) return 0;
+        int count = 0;
+        while (p != nullptr) {
+            if(p->value == v)
+                count++;
+            p = p->next;
+        }
+        return count;
     }
-
-
-
-
-
-
-
-
-
-
-    // Print the list values in reverse order,
-
-    // enclosed in angle brackets, e.g. < 4 3 2 1 >
 
     void printBackwards() const {
-
         cout << "< ";
-
-        // FILL IN MISSING CODE
-
+        Node<T>* temp = tailPtr;
+        while (temp != nullptr) {
+            cout << temp->value;
+            if (temp->prev != nullptr)
+                cout << " ";
+            temp = temp->next;
+        }
         cout << ">";
-
         cout << endl;
-
     }
-
-
-
-
-
-
-
-    // Insert v in the correct position
-
-    // in the list and update length
-
-    // Update headPtr or tailPtr if necessary.
-
-    // Avoid needless looping
-
-    // when v is the new biggest or the new smallest value.
 
     void insert(const T& v) {
+        Node<T>* toInsert = new Node<T>(v);
+        if (headPtr == nullptr) {
+            headPtr = toInsert;
+            tailPtr = toInsert;
+        }
+        else if (toInsert->value <= headPtr->value) {
+            toInsert->next = headPtr;
+            headPtr->prev = toInsert;
+            headPtr = toInsert;
+        }
+        else if (toInsert->value >= tailPtr->value) {
+            tailPtr->next = toInsert;
+            toInsert->prev = tailPtr;
+            tailPtr = toInsert;
+        }
+        else {
+            Node<T>* current = headPtr;
+            while (current->next != nullptr && current->next->value < toInsert->value) {
+                current = current->next;
+            }
+            toInsert->next = current->next;
+            toInsert->prev = current;
 
-        // FILL IN MISSING CODE
-
+            if (current->next != nullptr) {
+                current->prev = toInsert;
+            }
+            current->next = toInsert;
+        }
+        length++;
     }
-
-
-
-
-    // Return the list length
 
     int getLength() const {
-
-        // FILL IN MISSING CODE
-
+        return length;
     }
 
-
-
-
-    // Return true if the list is empty
 
     bool isEmpty() const {
-
-        // FILL IN MISSING CODE
-
+        if (headptr == nullptr) return true;
+        return false;
     }
 
-
-
-
-    // Set b to the biggest value or do nothing if the list is empty.
 
     void getBiggest(T& b) const {
-
-        // FILL IN MISSING CODE
-
+        if (tailPtr != nullptr)
+            b = tailPtr->value;
     }
 
-
-
-
-    // Set s to the smallest value or do nothing if the list is empty.
 
     void getSmallest(T& s) const {
-
-        // FILL IN MISSING CODE
-
+        if (headPtr != nullptr)
+            s = headPtr->value;
     }
 
-
-
-
-    // Remove the first occurence of v or do nothing if v isn't found.
-
-    // Your code should call findNodeWithValue.
 
     void remove(const T& v) {
+        if (headPtr == nullptr) return;
+        Node<T>* toDelete = findNodeWithValue(v);
+        if (toDelete == nullptr) return;                    
+        
+        if (toDelete == headPtr) {
+            headPtr = headPtr->next;
+            headPtr->prev = nullptr;
+        }
 
-        // FILL IN MISSING CODE
-
+        else if (toDelete == tailPtr) {
+            tailPtr = tailPtr->prev;
+            tailPtr->next = nullptr;
+        }
+        
+        else {
+            toDelete->prev->next = toDelete->next;
+            toDelete->next->prev = toDelete->prev;
+        }
+        
+        delete toDelete;
+        length--;
     }
-
-
-
-
 };
 
 template <class T>
@@ -280,11 +231,8 @@ template <class T>
 void testList(OrderedLinkedList<T> list) {
 
     cout << endl << "BEGIN TESTLIST" << endl;
-
     cout << "Length = " << list.getLength() << endl;
-
     cout << "Forwards: " << list << endl;
-
     cout << "Backwards: ";
 
     list.printBackwards();
@@ -292,162 +240,98 @@ void testList(OrderedLinkedList<T> list) {
     if (list.isEmpty()) cout << "The list is empty." << endl;
 
     else {
-
         T v;
 
         cout << "getSmallest returns ";
-
         list.getSmallest(v);
-
         cout << v << "." << endl;
-
         cout << "getBiggest returns ";
-
         list.getBiggest(v);
-
         cout << v << "." << endl;
 
     }
 
     try {
-
         cout << list[list.getLength()] << endl;
-
     }
 
     catch (logic_error e) {
-
         cout << e.what() << endl;
-
     }
 
     try {
-
         cout << list[-1] << endl;
-
     }
 
     catch (logic_error e) {
-
         cout << e.what() << endl;
-
     }
 
     for (int i = 0; i < list.getLength() / 2; i++)
-
         swap(list[i], list[list.getLength() - i - 1]);
 
     cout << "After reversing values " << list << endl;
-
     list.clear();
-
     cout << "After clear: " << list << endl;
-
     cout << "END TESTLIST" << endl << endl;
-
 }
 
 
 
 
-int main()
-
-{
-
+int main(){
     OrderedLinkedList<int> list;
-
     cout << "Initial list = " << list << endl;
-
     cout << "Initial list length = " << list.getLength() << endl;
-
     list.insert(11);
-
     list.insert(3);
-
     list.insert(5);
-
     list.insert(7);
-
     list.insert(6);
-
     list.insert(4);
-
     list.insert(2);
-
     list.insert(8);
-
     list.insert(5);
-
     list.insert(1);
-
     list.insert(0);
-
     list.insert(11);
-
     list.insert(10);
-
     list.insert(5);
-
     list.insert(11);
-
     list.insert(0);
-
     list.insert(8);
-
     list.insert(4);
 
     cout << "After insertions, list = " << list << endl;
-
     testList(list);
-
     cout << "After testList, list = " << list << endl;
-
     char response = 'n';
-
     int v = -1;
 
     do {
-
         cout << "Enter a value to remove or -1 to quit: ";
-
         cin >> v;
 
         if (v != -1) {
-
             cout << "old count: " << list.count(v) << endl;
-
             list.remove(v);
-
             cout << "new count: " << list.count(v) << endl;
-
             testList(list);
-
             cout << list << endl;
-
         }
-
     } while (v != -1);
 
     do {
-
         cout << "Enter a value to insert or -1 to quit: ";
-
         cin >> v;
 
         if (v != -1) {
-
             cout << "old count: " << list.count(v) << endl;
-
             list.insert(v);
-
             cout << "new count: " << list.count(v) << endl;
-
             testList(list);
-
             cout << list << endl;
-
         }
-
     } while (v != -1);
 
     cout << "Bye" << endl;
